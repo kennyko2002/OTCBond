3bdc74610a7e6da1a6492b51425358967ab256ed
kunyi ko <meowla2.kk@gmail.com>
2021-01-30 11:35:42 +0800

    manual

---
+++ twbond.py
@@ -0,0 +1,47 @@
+import datetime
+import pandas as pd
+import sys
+from tabula import read_pdf
+record=datetime.datetime.now()
+#等殖
+url='https://www.tpex.org.tw/storage/bond_zone/tradeinfo/govbond//' + record.strftime("%Y") + '/'+ record.strftime("%Y%m") + '/BDdys01a.'+ record.strftime("%Y%m%d") +'-C.xls'
+try:
+  x=pd.read_excel(url,skiprows=4,header=None,usecols=[0,1,2,3,9,10,11],names=['ID','Name','Duration','MaturityYear','High','Low','Average'])
+  y=x[x.Name.notna()]
+  y['recorddate']=record
+  y.to_csv('twbond',mode='a',index=False,header=False)
+except:
+  print("no data"+record.strftime("%Y%m%d"))
+#處所
+url='https://www.tpex.org.tw/storage/bond_zone/tradeinfo/govbond//' + record.strftime("%Y") + '/'+ record.strftime("%Y%m") + '/BDdcs001.'+ record.strftime("%Y%m%d") +'-C.xls'
+try:
+  x=pd.read_excel(url,skiprows=4,header=None,usecols=[0,1,2,3,9,10,11],names=['ID','Name','Duration','MaturityYear','High','Low','Average'])
+  y=x[x.Name.notna()]
+  y['recorddate']=record
+  y.to_csv('twbond',mode='a',index=False,header=False)
+except:
+  print("no data"+record.strftime("%Y%m%d"))
+
+
+#清單
+url='https://www.tpex.org.tw/storage/bond_zone/tradeinfo/govbond//' + record.strftime("%Y") + '/'+ record.strftime("%Y%m") + '/BDdys503.'+ record.strftime("%Y%m%d") +'-C.xls'  
+try:
+  x=pd.read_excel(url,skiprows=3,header=None,usecols=[0,1,2,3,4,5,13],names=['ID','Name','Maturity','MaturityYear','Duration','Coupon','Tyield'])
+  y=x[x.Name.notna()]
+  y['recorddate']=record
+  y.to_csv('twgovbondlist',mode='w',index=False)
+except:
+  print("no gov list to retrived"+record.strftime("%Y%m%d"))
+
+url='https://nweb.tpex.org.tw/bbg/bndcrvpx_'+ record.strftime("%Y%m%d") +'.pdf'
+try:
+  df = read_pdf(url,multiple_tables=False,pages = 'all',pandas_options={'header': None})
+  df=pd.DataFrame(df[0])
+  df.columns=['ID','Name','Maturity','CouponRating','Price','TYield']
+  df['Coupon']=df['CouponRating'].map(lambda x :x.split(' ')[0])
+  df['Rating']=df['CouponRating'].map(lambda x :x.split(' ')[1])
+
+  df.to_csv('twcorpbondlist',mode='w',index=False)
+except:
+  print("no data"+record.strftime("%Y%m%d"))
+
--- twbondmain.py
+++ twbondmain.py
@@ -7,29 +7,44 @@ from matplotlib import pyplot
 from matplotlib.font_manager import FontProperties
 bondfromcsv=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/twbond',names=['ID','Name','Duration','MaturityYear','High','Low','Average','recorddate'],parse_dates=['recorddate'],infer_datetime_format='%Y-%M-%D')
 bondfromcsv=bondfromcsv.astype({"ID":str, "Name":str})
+twgovbondlist=pd.read_csv('twgovbondlist')
+twcorpbondlist=pd.read_csv('twcorpbondlist')
 querybond=st.text_input('input the bond id')
-st.write("No trade record")
+
 if (st.button('Hit me')):
-  
+  intwgovbond=twgovbondlist[twgovbondlist['ID']==querybond]
+  intwcorpbond=twcorpbondlist[twcorpbondlist['ID']==querybond]
+  st.header("Bloomberg 公平市價")
+  st.table(intwgovbond)
   govtcondition=bondfromcsv.ID.map(lambda x: x.startswith('A'))
   target=bondfromcsv[bondfromcsv['ID']==querybond]
   if target.size==0:
     st.write("No trade record")
   else:
     pyplot.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+    st.subheader("本券近期成交記錄")
     st.table(target)
     targetduration=target['Duration'].values[-1]
     upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+1)) & (bondfromcsv['ID']!=querybond) & govtcondition]
     lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-1)) & (bondfromcsv['ID']!=querybond) & govtcondition]
-    
+    st.subheader("相近天期成交記錄")
+    col1,col2=st.betacolumns(2)
+    with col1:
+      st.table(upper)
+    with col2:
+      st.table(lower)
+    pyplot.ion()
     fig = pyplot.figure()
+    
     ax = fig.add_subplot(1,1,1)
-#    axes[0,0].tick_params(labelrotation=45)
-    ax.scatter(target.recorddate,target.Average,c='g',s=5**2)
-    ax.scatter(upper.recorddate,upper.Average,c='r',s=2**2)
-    ax.scatter(lower.recorddate,lower.Average,c='b',s=2**2)
+    #ax.tick_params(labelrotation=30)
+    s1=ax.scatter(target.recorddate,target.Average,c='g',s=5**2)
+    s2=ax.scatter(upper.recorddate,upper.Average,c='r',s=2**2)
+    s3=ax.scatter(lower.recorddate,lower.Average,c='b',s=2**2)
+    fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
+    fig.legend(
+    handles=(s1, s2, s3),labels=(querybond, 'longer bond', 'shortbond'),loc='upper right')
     
     
     st.pyplot(fig)
-    st.table(upper)
-
+   
