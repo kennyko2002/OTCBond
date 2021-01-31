3dbdb7d3f97834d42ff263252917bb53d9c20c95
kunyi ko <meowla2.kk@gmail.com>
2021-01-31 23:53:47 +0800

    manual

--- twbondmain.py
+++ twbondmain.py
@@ -52,39 +52,42 @@ if country=='Taiwan':
 	  else:
 	    pyplot.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
 	    st.subheader("本券近期成交記錄")
-	    st.table(target)
-	    
-	    if querybond.startswith('A'):
-	      targetduration=inbond.Duration.item()
-	    else :
-	      targetduration=target['Duration'].values[0]
+	    st.table(target)    
+	  if querybond.startswith('A'):
+	    targetduration=inbond.Duration.item()
+	    upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['recorddate']>=first_record_date) ]
+	    lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['recorddate']>=first_record_date) ]              
+	  else :
+	    targetduration=target['Duration'].values[0]
 	    upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['recorddate']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
 	    lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['recorddate']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
-	    st.subheader("相近(存續)天期成交記錄")
+	  st.subheader("相近(存續)天期成交記錄")
 	  #  col1,col2=st.beta_columns(2)
 	  #  with col1:
 	  #    st.table(upper[['ID','Name','Duration','Average']])
 	  #  with col2:
 	  #    st.table(lower[['ID','Name','Duration','Average']])
-	    st.table(upper[['ID','Name','Duration','Average','recorddate']])
-	    st.table(lower[['ID','Name','Duration','Average','recorddate']])
-	    fig = pyplot.figure()
+	  st.table(upper[['ID','Name','Duration','Average','recorddate']])
+	  st.table(lower[['ID','Name','Duration','Average','recorddate']])
+	  fig = pyplot.figure()
 	    
-	    ax = fig.add_subplot(1,1,1)
-	    ax.set_xlabel("日期")
-	    ax.set_ylabel('Yield%')
-	    
-	    #ax.tick_params(labelrotation=30)
-	    s1=ax.scatter(target.recorddate,target.Average,c='g',s=5**2,marker='D')
-	    s2=ax.scatter(upper.recorddate,upper.Average,c='r',s=2**2,marker='X')
-	    s3=ax.scatter(lower.recorddate,lower.Average,c='b',s=2**2,marker='X')
-	    ax.grid(axis='y')
-	    fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
-	    fig.legend(
+	  ax = fig.add_subplot(1,1,1)
+	  ax.set_xlabel("日期")
+	  ax.set_ylabel('Yield%')
+	  s2=ax.scatter(upper.recorddate,upper.Average,c='r',s=2**2,marker='X')
+	  s3=ax.scatter(lower.recorddate,lower.Average,c='b',s=2**2,marker='X')	    
+	  if target.size>0:
+	   s1=ax.scatter(target.recorddate,target.Average,c='g',s=5**2,marker='D')
+	   fig.legend(
 	    handles=(s1, s2, s3),labels=(querybond, 'longer bond', 'short bond'),loc='upper left',bbox_to_anchor=(0.13,0.87))
+	  
+	  ax.grid(axis='y')
+	  fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
+	  fig.legend(
+	    handles=(s2, s3),labels=('longer bond', 'short bond'),loc='upper left',bbox_to_anchor=(0.13,0.87))
 	    
 	    
-	    st.pyplot(fig)
+	  st.pyplot(fig)
 else:
         st.title('Philippines Bond Market')
         bondtype=st.radio('Govt/Corp?',('Govt','Corp'))
@@ -93,7 +96,10 @@ else:
           bondfromcsv=bondfromcsv.rename(columns={'D. Vol (MM)':'D.Vol(MM)'})
         else:
           bondfromcsv=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phicorp',names=['Global ID', 'Local ID', 'Domestic No.', 'Coupon Rate','YTM','Maturity', 'D.Vol(MM)', 'Last Yield','recorddate'],parse_dates=['recorddate'],infer_datetime_format='%Y-%M-%D')
-          bondfromcsv=bondfromcsv.rename(columns={'YTM':'YRS'})  
+          bondfromcsv=bondfromcsv.rename(columns={'YTM':'YRS'})
+          bondfromcsv['Ticker']=bondfromcsv['Global ID'].map(lambda x :x.split(' ')[0])
+
+
         bondfromcsv=bondfromcsv.astype({"YRS":float})
 	#bondfromcsv=bondfromcsv.astype({"ID":str, "Name":str})
 	#twgovbondlist=pd.read_csv('twgovbondlist')
@@ -126,14 +132,17 @@ else:
             st.write("No trade record")
          else:
             pyplot.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
-            st.subheader("本券近期成交記錄")
+            st.subheader("Trade Record for this issue ")
             st.table(target[['Global ID', 'Local ID', 'Domestic No.', 'YRS','Maturity', 'D.Vol(MM)', 'Last Yield','recorddate']])  
-	    
+            if bondtype=='Corp':
+              sameissuer=bondfromcsv[(bondfromcsv['Ticker']==querybond.split(' ')[0]) & (bondfromcsv['recorddate']>=first_record_date)]
+              st.subheader("Trade Record for same issuer ")
+              st.table(sameissuer[['Global ID', 'Local ID', 'Domestic No.', 'YRS','Maturity', 'D.Vol(MM)', 'Last Yield','recorddate']])
             targetduration=target['YRS'].values[0]
             #st.write(type(targetduration))
             upper=bondfromcsv[ (bondfromcsv['YRS']>targetduration) & (bondfromcsv['YRS']<(targetduration+duration_diff)) & (bondfromcsv['Global ID']!=querybond)  & (bondfromcsv['recorddate']>=first_record_date) ]
             lower=bondfromcsv[ (bondfromcsv['YRS']<targetduration) & (bondfromcsv['YRS']>(targetduration-duration_diff)) & (bondfromcsv['Global ID']!=querybond)  &(bondfromcsv['recorddate']>=first_record_date)]
-            st.subheader("相近(存續)天期成交記錄")
+            st.subheader("Similar tenor bond")
 	  #  col1,col2=st.beta_columns(2)
 	  #  with col1:
 	  #    st.table(upper[['ID','Name','Duration','Average']])
@@ -144,7 +153,7 @@ else:
             fig = pyplot.figure()
 	    
             ax = fig.add_subplot(1,1,1)
-            ax.set_xlabel("日期")
+            ax.set_xlabel("Date")
             ax.set_ylabel('Yield%')
 	    
 	    #ax.tick_params(labelrotation=30)
@@ -155,4 +164,5 @@ else:
             fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
             fig.legend(handles=(s1, s2, s3),labels=(querybond, 'longer bond', 'short bond'),loc='upper left',bbox_to_anchor=(0.13,0.87))	    
             st.pyplot(fig)
-
+       
+        
