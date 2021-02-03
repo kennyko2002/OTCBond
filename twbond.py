965db033bc08b5f9a9370d9b4ea5b8a8aaa91576
kunyi ko <meowla2.kk@gmail.com>
2021-02-04 01:11:08 +0800

    auto update 'date'

--- twbond.py
+++ twbond.py
@@ -3,6 +3,8 @@ import pandas as pd
 import sys
 from tabula import read_pdf
 import numpy as np
+import urllib.request
+from bs4 import BeautifulSoup
 record=datetime.date.today()
 #等殖
 url='https://www.tpex.org.tw/storage/bond_zone/tradeinfo/govbond//' + record.strftime("%Y") + '/'+ record.strftime("%Y%m") + '/BDdys01a.'+ record.strftime("%Y%m%d") +'-C.xls'
@@ -68,5 +70,21 @@ try:
   df.to_csv('phicorp',mode='a',index=False,header=None) 
 except:
   print("no corp data"+record.strftime("%Y%m%d"))
+
+
+urls=['https://www.tpex.org.tw/web/bond/publish/corporate_bond_search/memo_professional.php?l=zh-tw','https://www.tpex.org.tw/web/bond/publish/corporate_bond_search/memo.php?l=zh-tw']
+for url in urls:
+  response = urllib.request.urlopen(url)
+  html = response.read()
+  sp = BeautifulSoup(html) 
+  tbls=sp.find_all('table')
+  x=tbls[0].find_all('td')
+  y=[]
+  for s in x : 
+    y.append(s.text)
+  y_sublists = [y[i:i+9] for i in range(0, len(y), 9)]
+  yy=pd.DataFrame(y_sublists )
+  yy=yy[[0,1,2]]
+  yy.to_csv('corplistpro',mode='a',index=False,header=None)
 
 
