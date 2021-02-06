e4f8407272e1b3395474ff62135f60983642eb01
kunyi ko <meowla2.kk@gmail.com>
2021-02-06 22:00:26 +0800

    manual

--- twbondmain.py
+++ twbondmain.py
@@ -12,30 +12,59 @@ start_date=30
 first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
 pd.options.display.float_format='{:,.3f}'.format
 pyplot.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
-st.markdown("""	<style>
+
+
+def mycss():           
+  st.markdown("""	<style>		
+        .main {background:linear-gradient(to top right,#c3c95a,#c4b281);}
+table {
+background:white;
+background:linear-gradient(to bottom right,rgba(255,255,255,0.7),rgba(255,255,255,0.3) );
+border-radius:50px; 
+font-weight:bold;
+white-space:nowrap;
+	}
+
+h1,h2,h3{
+text-align:center;}
+	</style>
+	""", unsafe_allow_html=True)
+
+  st.markdown("""	<style>
 	table td:nth-child(1) {
 	    display: none
 	}
 	table th:nth-child(1) {
 	    display: none
 	}
+.table-bottom-left > div:nth-child(1){
+	    display: none;            
+	}
+.table-bottom-left{
+background-color:#c3c95a;	
 	</style>
 	""", unsafe_allow_html=True)
+
+
+
+
+mycss()
 if country=='Taiwan':
 	st.title('台債市場交易查詢')
-	bondfromcsv=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/twbond',names=['ID','Name','Duration','MaturityYear','High','YTM','VolumeE','recorddate'],thousands=r',',parse_dates=['recorddate'],infer_datetime_format='%Y-%M-%D')
+	bondfromcsv=st.cache(pd.read_csv)('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/twbond',names=['ID','Name','Duration','MaturityYear','High','YTM','VolumeE','Trade_date'],thousands=r',',parse_dates=['Trade_date'],infer_datetime_format='%Y-%M-%D')
 	bondfromcsv=bondfromcsv.astype({"ID":str, "Name":str})
-	twgovbondlist=pd.read_csv('twgovbondlist')
-	twcorpbondlist=pd.read_csv('twcorpbondlist')
-	corplistpro=pd.read_csv('corplistpro',names=['ID','Ticker','FullName'])
+	twgovbondlist=st.cache(pd.read_csv)('twgovbondlist')
+	twcorpbondlist=st.cache(pd.read_csv)('twcorpbondlist')
+	corplistpro=st.cache(pd.read_csv)('corplistpro',names=['ID','Ticker','FullName'])
 
 	bondfromcsv=pd.merge(bondfromcsv,twcorpbondlist,how='left',on='ID')
 	bondfromcsv=pd.merge(bondfromcsv,corplistpro,how='left',on='ID')
-	bondfromcsv=bondfromcsv[['ID','Name_x','Rating','Duration','MaturityYear','YTM','VolumeE','recorddate','FullName']]
-	bondfromcsv=bondfromcsv.fillna('twAAA')
+	bondfromcsv=bondfromcsv[['ID','Name_x','Rating','Duration','MaturityYear','YTM','VolumeE','Trade_date','FullName']]
+	#bondfromcsv=bondfromcsv.fillna('twAAA')
+	bondfromcsv.Rating.fillna('twAAA',inplace=True)
 	bondfromcsv.rename(columns={"Name_x":"Name"},inplace=True)
-	bondfromcsv.sort_values(by='recorddate',ascending=False,inplace=True)
-	querytype=st.sidebar.radio('query type',('By ID','By Issuer','By Tenor'))	
+	bondfromcsv.sort_values(by='Trade_date',ascending=False,inplace=True)
+	querytype=st.sidebar.radio('Query type?',('By ID','By Issuer','By Tenor'))	
 	if querytype=='By ID':
 	  querybond=st.sidebar.text_input('請輸入券號')
 	  duration_diff=st.sidebar.slider("存續期間差異", min_value=0.0, max_value=5.0, value=1.0,step=0.1)
@@ -55,38 +84,38 @@ if country=='Taiwan':
 	    st.header("Bloomberg 公平市價")
 	    st.table(inbond)
 	    first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
-	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['recorddate']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
-	    target=bondfromcsv[(bondfromcsv['ID']==querybond) & (bondfromcsv['recorddate']>=first_record_date)]
+	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['Trade_date']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
+	    target=bondfromcsv[(bondfromcsv['ID']==querybond) & (bondfromcsv['Trade_date']>=first_record_date)]
 	    if target.size==0:
 	      st.write("No trade record")
 	    else:
 	      st.subheader("本券近期成交記錄")
-	      st.table(target)    
+	      st.table(target[['ID','Name','Duration','YTM','Trade_date']])    
 	    if querybond.startswith('A'):
 	      targetduration=inbond.Duration.item()
-	      upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['recorddate']>=first_record_date) ]
-	      lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['recorddate']>=first_record_date) ]              
+	      upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['Trade_date']>=first_record_date) ]
+	      lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['Trade_date']>=first_record_date) ]              
 	    else :
 	      targetduration=target['Duration'].values[0]
-	      upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['recorddate']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
-	      lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['recorddate']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
+	      upper=bondfromcsv[ (bondfromcsv['Duration']>targetduration) & (bondfromcsv['Duration']<(targetduration+duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition & (bondfromcsv['Trade_date']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
+	      lower=bondfromcsv[ (bondfromcsv['Duration']<targetduration) & (bondfromcsv['Duration']>(targetduration-duration_diff)) & (bondfromcsv['ID']!=querybond) & govtcondition &(bondfromcsv['Trade_date']>=first_record_date) & (bondfromcsv['Rating']==target['Rating'].values[0])]
 	    st.subheader("相近(存續)天期成交記錄")
 	  #  col1,col2=st.beta_columns(2)
 	  #  with col1:
 	  #    st.table(upper[['ID','Name','Duration','YTM']])
 	  #  with col2:
 	  #    st.table(lower[['ID','Name','Duration','YTM']])
-	    st.table(upper[['ID','Name','Duration','YTM','recorddate']])
-	    st.table(lower[['ID','Name','Duration','YTM','recorddate']])
+	    st.table(upper[['ID','Name','Duration','YTM','Trade_date']])
+	    st.table(lower[['ID','Name','Duration','YTM','Trade_date']])
 	    fig = pyplot.figure()
 	    
 	    ax = fig.add_subplot(1,1,1)
 	    ax.set_xlabel("日期")
 	    ax.set_ylabel('YTM%')
-	    s2=ax.scatter(upper.recorddate,upper.YTM,c='r',s=2**2,marker='X')
-	    s3=ax.scatter(lower.recorddate,lower.YTM,c='b',s=2**2,marker='X')	    
+	    s2=ax.scatter(upper.Trade_date,upper.YTM,c='r',s=2**2,marker='X')
+	    s3=ax.scatter(lower.Trade_date,lower.YTM,c='b',s=2**2,marker='X')	    
 	    if target.size>0:
-	      s1=ax.scatter(target.recorddate,target.YTM,c='g',s=5**2,marker='D')
+	      s1=ax.scatter(target.Trade_date,target.YTM,c='g',s=5**2,marker='D')
 	      fig.legend(
 	    handles=(s1, s2, s3),labels=(querybond, 'longer bond', 'shorter bond'),loc='upper left',bbox_to_anchor=(0.13,0.87))
 	    else:
@@ -97,39 +126,40 @@ if country=='Taiwan':
 	    st.pyplot(fig)
 	elif querytype=='By Issuer':
 	  issuername=st.sidebar.selectbox('By Issuer',bondfromcsv.FullName.unique())
+	  start_date=st.sidebar.slider("資料期間", min_value=0, max_value=90, value=30,step=1)
+	  first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
 	  if(st.sidebar.button('查詢')):	  
-	    st.table( bondfromcsv[(bondfromcsv.FullName==issuername) & (bondfromcsv['recorddate']>=first_record_date)])
+	    st.table( bondfromcsv[(bondfromcsv.FullName==issuername) & (bondfromcsv['Trade_date']>=first_record_date)])
 	else:
 	  tenor=st.sidebar.slider("債券天期", min_value=0.0, max_value=30.0, value=(1.0,5.0),step=0.1)
-	  start_date=st.sidebar.slider("資料期間", min_value=0, max_value=90, value=30,step=1)
+	  start_date=st.sidebar.slider("資料期間", min_value=0, max_value=90, value=30,step=1)	  
 	  if (st.sidebar.button('查詢')):
 	    first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
-	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['recorddate']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
-	    target=bondfromcsv[(bondfromcsv['Duration']>tenor[0]) & (bondfromcsv['Duration']<tenor[1]) & (bondfromcsv['recorddate']>=first_record_date)]
+	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['Trade_date']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
+	    target=bondfromcsv[(bondfromcsv['Duration']>tenor[0]) & (bondfromcsv['Duration']<tenor[1]) & (bondfromcsv['Trade_date']>=first_record_date)]
 	    if target.size==0:
 	      st.write("No trade record")
 	    else:
-	      st.subheader("本券近期成交記錄")
-	      st.table(target)    
+	      st.subheader("近期成交記錄")
+	      st.table(target[['ID','Name','Duration','YTM','Trade_date']])    
 else:
         st.title('Philippines Bond Market')
         bondtype=st.sidebar.radio('Govt/Corp?',('Govt','Corp'))
         if bondtype=='Govt':
-          bondfromcsv=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phigovt',thousands=r',',names=['Global ID', 'Local ID', 'Domestic No.', 'CPN','YRS','Maturity', 'D. Vol (MM)', 'YTM','recorddate'],parse_dates=['recorddate'],infer_datetime_format='%Y-%M-%D')
+          bondfromcsv=st.cache(pd.read_csv)('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phigovt',thousands=r',',names=['Global ID', 'Local ID', 'Domestic No.', 'CPN','YRS','Maturity', 'D. Vol (MM)', 'YTM','Trade_date'],parse_dates=['Trade_date'],infer_datetime_format='%Y-%M-%D')
           bondfromcsv=bondfromcsv.rename(columns={'D. Vol (MM)':'D.Vol(MM)'})
           querytype=st.sidebar.radio('query type',('By ID','By Tenor'))
         else:
-          bondfromcsv=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phicorp',thousands=r',',names=['Global ID', 'Local ID', 'Domestic No.', 'Coupon Rate','YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate'],parse_dates=['recorddate'],infer_datetime_format='%Y-%M-%D')
-          #bondfromcsv=bondfromcsv.rename(columns={'YTM':'YRS'})
+          bondfromcsv=st.cache(pd.read_csv,allow_output_mutation=True)('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phicorp',thousands=r',',names=['Global ID', 'Local ID', 'Domestic No.', 'Coupon Rate','YRS','Maturity', 'D.Vol(MM)', 'YTM','Trade_date'],parse_dates=['Trade_date'],infer_datetime_format='%Y-%M-%D')
           bondfromcsv['Ticker']=bondfromcsv['Global ID'].map(lambda x :x.split(' ')[0])
-          phi_issuer_list=pd.read_csv('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phitest',names=['corpname','key'])
+          phi_issuer_list=st.cache(pd.read_csv)('https://raw.githubusercontent.com/kennyko2002/OTCBond/master/phitest',names=['corpname','key'])
           bondfromcsv['key']=bondfromcsv['Local ID'].str.replace(' ','')
           bondfromcsv=bondfromcsv.join(phi_issuer_list.set_index('key'),how='left',on='key')
   
-          querytype=st.sidebar.radio('query type',('By ID','By Issuer','By Tenor'))
+          querytype=st.sidebar.radio('Query type?',('By ID','By Issuer','By Tenor'))
         if querytype=='By ID':
           bondfromcsv=bondfromcsv.astype({"YRS":float})
-          bondfromcsv.sort_values(by='recorddate',ascending=False,inplace=True)
+          bondfromcsv.sort_values(by='Trade_date',ascending=False,inplace=True)
           #querybond=st.sidebar.selectbox('Global ID',np.sort(bondfromcsv['Global ID'].unique()))
           querybond=st.sidebar.selectbox('Global ID',bondfromcsv.sort_values('YRS')['Global ID'].unique())
           duration_diff=st.sidebar.slider("tenor_diff", min_value=0.0, max_value=5.0, value=1.0,step=0.1)
@@ -137,41 +167,37 @@ else:
 
  
 
-          if(st.sidebar.button('summit')):   
+          if(st.sidebar.button('submit')):   
            first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
-	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['recorddate']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
-           target=bondfromcsv[(bondfromcsv['Global ID']==querybond) & (bondfromcsv['recorddate']>=first_record_date)]
+	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['Trade_date']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
+           target=bondfromcsv[(bondfromcsv['Global ID']==querybond) & (bondfromcsv['Trade_date']>=first_record_date)]
            if target.size==0:
               st.write("No trade record")
            else:
               
-              st.subheader("Trade Record for this issue ")
-              st.table(target[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']])  
-              if bondtype=='Corp':
-                sameissuer=bondfromcsv[(bondfromcsv['Ticker']==querybond.split(' ')[0]) & (bondfromcsv['recorddate']>=first_record_date)]
-                st.subheader("Trade Record for same issuer ")
-                st.table(sameissuer[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']])
+              st.subheader("Trade record for this issue ")
+              st.table(target[['Global ID', 'Local ID', 'YRS', 'D.Vol(MM)', 'YTM','Trade_date']])
               targetduration=target['YRS'].values[0]
               #st.write(type(targetduration))
-              upper=bondfromcsv[ (bondfromcsv['YRS']>targetduration) & (bondfromcsv['YRS']<(targetduration+duration_diff)) & (bondfromcsv['Global ID']!=querybond)  & (bondfromcsv['recorddate']>=first_record_date) ]
-              lower=bondfromcsv[ (bondfromcsv['YRS']<targetduration) & (bondfromcsv['YRS']>(targetduration-duration_diff)) & (bondfromcsv['Global ID']!=querybond)  &(bondfromcsv['recorddate']>=first_record_date)]
+              upper=bondfromcsv[ (bondfromcsv['YRS']>targetduration) & (bondfromcsv['YRS']<(targetduration+duration_diff)) & (bondfromcsv['Global ID']!=querybond)  & (bondfromcsv['Trade_date']>=first_record_date) ]
+              lower=bondfromcsv[ (bondfromcsv['YRS']<targetduration) & (bondfromcsv['YRS']>(targetduration-duration_diff)) & (bondfromcsv['Global ID']!=querybond)  &(bondfromcsv['Trade_date']>=first_record_date)]
               st.subheader("Similar tenor bond")
 	  #  col1,col2=st.beta_columns(2)
 	  #  with col1:
 	  #    st.table(upper[['ID','Name','Duration','YTM']])
 	  #  with col2:
 	  #    st.table(lower[['ID','Name','Duration','YTM']])
-              st.table(upper[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']])
-              st.table(lower[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']])
+              st.table(upper[['Global ID', 'Local ID', 'YRS', 'D.Vol(MM)', 'YTM','Trade_date']])
+              st.table(lower[['Global ID', 'Local ID', 'YRS', 'D.Vol(MM)', 'YTM','Trade_date']])
            fig = pyplot.figure()
            ax = fig.add_subplot(1,1,1)
            ax.set_xlabel("Date")
            ax.set_ylabel('YTM%')
 	    
 	    #ax.tick_params(labelrotation=30)
-           s1=ax.scatter(target.recorddate,target['YTM'],c='g',s=5**2,marker='D')
-           s2=ax.scatter(upper.recorddate,upper['YTM'],c='r',s=2**2,marker='X')
-           s3=ax.scatter(lower.recorddate,lower['YTM'],c='b',s=2**2,marker='X')
+           s1=ax.scatter(target.Trade_date,target['YTM'],c='g',s=5**2,marker='D')
+           s2=ax.scatter(upper.Trade_date,upper['YTM'],c='r',s=2**2,marker='X')
+           s3=ax.scatter(lower.Trade_date,lower['YTM'],c='b',s=2**2,marker='X')
            ax.grid(axis='y')
            fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
            fig.legend(handles=(s1, s2, s3),labels=(querybond, 'longer bond', 'short bond'),loc='upper left',bbox_to_anchor=(0.13,0.87))	    
@@ -180,20 +206,26 @@ else:
            issuername=st.sidebar.selectbox('By Issuer',bondfromcsv.corpname.unique())
            start_date=st.sidebar.slider("data period", min_value=0, max_value=90, value=30,step=1)
            first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
-           outputtable=bondfromcsv[(bondfromcsv.corpname==issuername) &(bondfromcsv['recorddate']>=first_record_date)]
-           outputtable=outputtable[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']]
-           st.table( outputtable.sort_values(by='recorddate',ascending=False))
+           outputtable=bondfromcsv[(bondfromcsv.corpname==issuername) &(bondfromcsv['Trade_date']>=first_record_date)]
+           outputtable=outputtable[['Global ID', 'Local ID', 'YRS', 'D.Vol(MM)', 'YTM','Trade_date']]
+           if outputtable.size>0:         
+             #st.table( outputtable.sort_values(by='Trade_date',ascending=False))
+              st.dataframe(outputtable.sort_values(by='Trade_date',ascending=False))
+           else:
+             st.write("No trade record")
         else :
            tenor=st.sidebar.slider("tenor", min_value=0.0, max_value=30.0, value=(1.0,5.0),step=0.1)
            start_date=st.sidebar.slider("data period", min_value=0, max_value=90, value=30,step=1)
            if (st.sidebar.button('Query')):
              first_record_date=np.datetime64((datetime.date.today()-datetime.timedelta(start_date)))
-	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['recorddate']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
-             target=bondfromcsv[(bondfromcsv['YRS']>tenor[0]) & (bondfromcsv['YRS']<tenor[1]) & (bondfromcsv['recorddate']>=first_record_date)]
+	#  target=bondfromcsv[bondfromcsv['ID']==querybond & bondfromcsv['Trade_date']>=(np.datetime64((datetime.date.today()-datetime.timedelta(start_date))))]
+             target=bondfromcsv[(bondfromcsv['YRS']>tenor[0]) & (bondfromcsv['YRS']<tenor[1]) & (bondfromcsv['Trade_date']>=first_record_date)]
              if target.size==0:
                st.write("No trade record")
              else:
                st.subheader("trade record")
-               target=target[['Global ID', 'Local ID', 'YRS','Maturity', 'D.Vol(MM)', 'YTM','recorddate']]
-               st.table(target.sort_values(by='recorddate',ascending=False))           
+               target=target[['Global ID', 'Local ID', 'YRS', 'D.Vol(MM)', 'YTM','Trade_date']]
+               st.table(target.sort_values(by='Trade_date',ascending=False))
+
             
+
