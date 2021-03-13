6f31465a53f5daa71b4af17d5260bf0ad622a146
kenny <meowla2.kk@gmail.com>
2021-03-13 19:49:02 +0800

    man

---
+++ update-repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+status_log=$(git status -sb)
+# 這裡使用的是 master 分支，根據需求自行修改
+if [ "$status_log" == "## master...origin/master" ];then
+  echo "nothing to commit, working tree clean"
+else
+  git add .&&git commit -m "update by github actions"&&git push origin master
+fi
